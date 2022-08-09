0d8787eab6e96ca48b3bf7a1964009b214207a0b
Fatima <72432366+fatima-amri@users.noreply.github.com>
2022-08-09 15:11:15 -0700

    Update migration - GitHub.py

--- migration - GitHub.py
+++ migration - GitHub.py
@@ -50,44 +50,76 @@ for i in range(len(linkDis)):
 
 
 ######################################### controller checkup ###########################################
-P_peak=200 #watt
-P_idle=100 #watt
-c_m= 100 #number of servers in each DC
-eta= 1.2
-a=0.3*P_peak*c_m*eta
-b=P_peak*c_m*eta
+def controller_checkup ():
+    P_peak = 200  # watt
+    P_idle = 100  # watt
+    c_m = 100  # number of servers in each DC
+    eta = 1.2
+    a = 0.3 * P_peak * c_m * eta
+    b = P_peak * c_m * eta
 
-#available RE resources at the begining of the migration cycle
-RE_resources=[]
-for dc in range(numOfNode):
-    zeta_s= np.random.randint(a,b)
-    RE_resources.append(zeta_s)
-print("available RE = ", RE_resources)
-#m= np.mean(RE_resources)
-#print(m)
-
-# amount of workload in each DC
-#w=np.random.poisson(lam=2, size=14)
-Vec_Pdyna=[]
-for dc in range(numOfNode):
-    cpu_req= np.random.randint(1,3,size=100)
-    total_cpu= sum(cpu_req)
-    U_mn= total_cpu/1600
-    numOfServer= math.ceil(total_cpu/16)
-    P_dynamic = (P_peak - P_idle) * U_mn * numOfServer
-    Vec_Pdyna.append(P_dynamic)
-print("amount of P dynamic in each DC = ", Vec_Pdyna)
-
-P_static= (P_idle+(eta-1)*P_peak)*100 #is equal to 140 watt and 100 server in total in a DC
-print(P_static)
-P_dynamic1= np.array(Vec_Pdyna)
-P_dc= list(P_static + P_dynamic1)
-print("workload in each DC before migration= ", P_dc)
+    # available RE resources at the beginning of the migration cycle
+    RE_resources = []
+    for dc in range(numOfNode):
+        zeta_s = np.random.randint(a, b)
+        RE_resources.append(zeta_s)
+    print("available RE = ", RE_resources)
 
 
-List_D=[]
-for dc in range(numOfNode):
-    if P_dc[dc]> RE_resources[dc]:
-        List_D.append(dc)
-print("DC with insufficient RE = ", List_D)
+    # amount of workload in each DC
+    Vec_Pdyna = []
+    for dc in range(numOfNode):
+        cpu_req = np.random.randint(1, 3, size=400)
+        total_cpu = sum(cpu_req)
+        U_mn = total_cpu / 1600
+        numOfServer = math.ceil(total_cpu / 16)
+        P_dynamic = (P_peak - P_idle) * U_mn * numOfServer
+        Vec_Pdyna.append(P_dynamic)
+    print("amount of P dynamic in each DC = ", Vec_Pdyna)
+
+    P_static = (P_idle + (eta - 1) * P_peak) * 100  # is equal to 140 watt and 100 server in total in a DC
+    print(P_static)
+    P_dynamic1 = np.array(Vec_Pdyna)
+    P_dc = list(P_static + P_dynamic1)
+    print("workload in each DC before migration= ", P_dc)
+
+    List_D = []
+    for dc in range(numOfNode):
+        if P_dc[dc] > RE_resources[dc]:
+            List_D.append(dc)
+    print("DC with insufficient RE = ", List_D)
+    return List_D
+
+######################################### MAB ###########################################
+
+def MAB(List_D, numOfNode):
+    ListOfDC = list(range(numOfNode))
+    ListOfArms= list(set(ListOfDC)-set(List_D))
+    beta = 0.001 #dollar
+    round = 0
+    cost = np.zeros((len(List_D),len(ListOfArms)))
+    for dc in List_D:
+        for arm in ListOfArms:
+            (T, Amp)= R_OG(dc, arm) #receiving the number of used transponders and amplifiers in the path
+            #Total_c= (alpha[arm]* fi) + (beta* bw) + (gamma[]* T)+ (sigma[] * Amp)
+            #cost[dc][arm]= Total_c
+
+    return
+
+######################### ROUTING AND OPTICAL GROOMING ###############################
+
+#number of EDFA amplifiers in each physical link :
+EA=np.zeros((numOfNode, numOfNode))
+for i in range(len(linkDis)):
+    for j in range(len(linkDis)):
+        if linkDis[i][j] != 0:
+            temp = (linkDis[i][j] / 80) - 1
+            EA[i][j] = math.floor(temp) + 2
+#print(EA)
+
+
+def R_OG (src, des):
+
+
+    return
 
