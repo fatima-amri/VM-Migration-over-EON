8b969d07bbe41cb3a7124d8475f4b178062975dd
Fatima <72432366+fatima-amri@users.noreply.github.com>
2023-05-05 18:53:23 -0700

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,70 @@
+from datetime import datetime
+
+start = datetime.now()
+
+vec_c_before = []
+vec_c_after = []
+improvement_lst = []
+mig_mat =[]
+mig_vec =[]
+reg = []
+teta_vec =[]
+costNet_vec =[]
+
+# As we used some random variables, we repeat the simulation 100 times.
+for r in range(100): 
+    (List_D, P_dc, RE_resources, all_cpu_reqs, initial_cost) = controller_checkup()
+    (COST, BEcostOfDC, reg_vec, teta, costNet) = MAB(numOfNode, List_D, P_dc, RE_resources, all_cpu_reqs)
+
+    teta_vec.append(teta)
+
+    costNetsum = np.sum(costNet)
+    costNet_vec.append(costNetsum)
+
+    reg1 = list(np.cumsum(reg_vec))
+    reg.append(reg1)
+
+
+    total_BEc_before = cost_cal(initial_cost)
+    vec_c_before.append(total_BEc_before)
+
+    total_BEc_after = cost_cal(BEcostOfDC)
+    vec_c_after.append(total_BEc_after)
+
+    Topo_migCost = np.sum(COST)
+    mig_vec.append(Topo_migCost)
+
+    if total_BEc_after < total_BEc_before:
+        print("yes")
+        #print("the cost has been decreased by ", total_c_before - total_c_after)
+        #print("so we have ", (100 - ((total_c_after / total_c_before) * 100)), " percent improvement (decrease)")
+        p = (100 - ((total_BEc_after / total_BEc_before) * 100))
+        improvement_lst.append(p)
+
+
+
+for m in range(1):
+    l = len(reg[m])
+    if round(mean_teta)-2 <= l <= round(mean_teta)+2:
+        print(reg[m])
+
+
+mean_before = average(vec_c_before)
+mean_after = average(vec_c_after)
+print("cost avg before migration = ", mean_before)
+print("cost avg after migration =  ", mean_after)
+
+mean_percentage = average(improvement_lst)
+print("in average the improvement is ", mean_percentage, " percent")
+
+mean_migCost = average(mig_vec)
+print("avg of migration cost = ", mean_migCost)
+
+mean_netCost = average(costNet_vec)
+print("avg of network elements' cost = ", mean_netCost)
+
+Total_cost = mean_after + mean_migCost
+print("Final Total Cost = ",Total_cost)
+
+
+print("time = ", datetime.now()-start)
